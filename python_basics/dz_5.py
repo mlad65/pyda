e1a70c88ab1ff88e4df0f8497d70aa1574fc2244
mlad65 <64573963+mlad65@users.noreply.github.com>
2021-02-05 22:06:34 +1100

    reworked homework

--- python_basics/dz_5.py
+++
@@ -1,5 +0,0 @@
-number = 123411
-left = str(number)[:3]
-right = str(number)[3:]
-print('Счастливый билет' if int(left[0]) + int(left[1]) + int(left[2]) == int(right[0]) + int(right[1]) + int(right[2]) else 'Несчастливый билет')
-    
